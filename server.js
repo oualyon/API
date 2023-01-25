98ca7732325bff6e7fc3299a6ea6d75155bedc8d
stephane.duboze <stephane.duboze@ynov.com>
2023-01-25 16:15:22 +0100

    Category handler

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
+
 const port = process.env.PORT || 5000;
 var hostname = "localhost" ; 
 
+app.use(cors())
 // const csvtojsonRouter = require('./routes/convert')
 var csvtojson = require('./routes/convert');
 app.use('/csv',csvtojson);
+var geojson = require('./routes/convert');
+app.use('/geo',geojson);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
