85c2caeb7fa7c4808a3adb317192d4d298eaed0d
stephane.duboze <stephane.duboze@ynov.com>
2023-02-15 17:29:22 +0100

    Postal code filter

--- routes/convert.js
+++ routes/convert.js
@@ -2,7 +2,7 @@ const CSVToJSON = require('csvtojson')
 var express = require('express');
 const router = express.Router();
 const activityFile ="file/Activities.geojson" ; 
-var fs = require('fs');
+
 
  router.get('/', function(req, res) {
    fs.readFile(activityFile, function(err, data) {
@@ -16,14 +16,18 @@ var fs = require('fs');
    });
  });
 
-router.get('/:category', function(req, res) {
+ router.get('/:category/:postalCode?', function(req, res) {
   fs.readFile(activityFile, function(err, data) {
     if (err) {
       res.send(err);
     } else {
       var jsonData = JSON.parse(data);
       var filteredFeatures = jsonData.features.filter(function(feature) {
-        return feature.properties.Category === req.params.category;
+        if (req.params.postalCode) {
+          return feature.properties.Category === req.params.category && feature.properties["Code Postal"] === req.params.postalCode;
+        } else {
+          return feature.properties.Category === req.params.category;
+        }
       });
       jsonData.features = filteredFeatures;
       res.setHeader('Content-Type', 'application/json');
@@ -31,5 +35,6 @@ router.get('/:category', function(req, res) {
     }
   });
 });
+
 
 module.exports = router;
