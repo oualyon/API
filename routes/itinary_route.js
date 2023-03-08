c931a364c71f53d6e671400540fe6639513578df
stephane.duboze <stephane.duboze@ynov.com>
2023-03-08 17:29:34 +0100

    Barathon 2

--- routes/itinary_route.js
+++ routes/itinary_route.js
@@ -2,6 +2,7 @@ var express = require('express');
 const router = express.Router();
 const gpx = "file/rhone.geojson" ; 
 const bar = "file/bar.geojson" ; 
+const bar_guillotiere_to_vieuxlyon = "file/bar_guillotiere_to_vieuxlyon.geojson" ; 
 
 var fs = require('fs');
 
@@ -30,6 +31,16 @@ router.get('/Bar', function(req, res) {
     });
   });
   
-
+  router.get('/bar_guillotiere_to_vieuxlyon', function(req, res) {
+    fs.readFile(bar_guillotiere_to_vieuxlyon, function(err, data) {
+      if (err) {
+        res.send(err);
+      } else {
+        var jsonData = JSON.parse(data);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(jsonData));
+      }
+    });
+  });
 
 module.exports = router;
