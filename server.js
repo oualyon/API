f0c0e797297e6922dc61094dbcdfba6321e94f58
stephane.duboze <stephane.duboze@ynov.com>
2023-03-29 15:26:39 +0200

    fix 1

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const app = express()
-var csvtojson = require('./routes/convert');
-var etl_launch = require('./routes/etl_launch');
 var cors = require('cors')
 
 const port = process.env.PORT || 5000;
