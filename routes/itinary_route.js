449e2fcbb054157d0f927a555cfba48522486f6c
stephane.duboze <stephane.duboze@ynov.com>
2023-04-19 12:08:48 +0200

    Street art et monument

--- routes/itinary_route.js
+++ routes/itinary_route.js
@@ -1,14 +1,19 @@
 var express = require('express');
 const router = express.Router();
-const gpx = "file/rhone.geojson" ; 
-const bar = "file/bar.geojson" ; 
-const bar_guillotiere_to_vieuxlyon = "file/bar_guillotiere_to_vieuxlyon.geojson" ; 
+const balade_rhone = "file/rhone.geojson" ; 
+const barathonA = "file/bar.geojson" ; 
+const barathonB = "file/bar_guillotiere_to_vieuxlyon.geojson" ; 
+const Presquile_historique = "file/Presquile_historique.geojson" ; 
+const StreetArt_1 = "file/StreetArt.geojson"
+const StreetArt_2 = "file/StreetArt2.geojson"
+const  Vieux_Lyon_Historique = "file/Vieux_Lyon_Historique"
+
 
 var fs = require('fs');
 
 
-router.get('/gpx', function(req, res) {
-  fs.readFile(gpx, function(err, data) {
+router.get('/balade_rhone', function(req, res) {
+  fs.readFile(balade_rhone, function(err, data) {
     if (err) {
       res.send(err);
     } else {
@@ -19,8 +24,8 @@ router.get('/gpx', function(req, res) {
   });
 });
 
-router.get('/Bar', function(req, res) {
-    fs.readFile(bar, function(err, data) {
+router.get('/barathonA', function(req, res) {
+    fs.readFile(barathonA, function(err, data) {
       if (err) {
         res.send(err);
       } else {
@@ -31,8 +36,54 @@ router.get('/Bar', function(req, res) {
     });
   });
   
-  router.get('/bar_guillotiere_to_vieuxlyon', function(req, res) {
-    fs.readFile(bar_guillotiere_to_vieuxlyon, function(err, data) {
+  router.get('/barathonB', function(req, res) {
+    fs.readFile(barathonB, function(err, data) {
+      if (err) {
+        res.send(err);
+      } else {
+        var jsonData = JSON.parse(data);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(jsonData));
+      }
+    });
+  });
+  router.get('/Balade_Presquile_historique', function(req, res) {
+    fs.readFile(Presquile_historique, function(err, data) {
+      if (err) {
+        res.send(err);
+      } else {
+        var jsonData = JSON.parse(data);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(jsonData));
+      }
+    });
+  });
+
+  router.get('/Balade_StreetArt_1', function(req, res) {
+    fs.readFile(StreetArt_1, function(err, data) {
+      if (err) {
+        res.send(err);
+      } else {
+        var jsonData = JSON.parse(data);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(jsonData));
+      }
+    });
+  });
+
+  router.get('/Balade_StreetArt_2', function(req, res) {
+    fs.readFile(StreetArt_2, function(err, data) {
+      if (err) {
+        res.send(err);
+      } else {
+        var jsonData = JSON.parse(data);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(jsonData));
+      }
+    });
+  });
+  router.get('/Balade_Vieux_Lyon_Historique', function(req, res) {
+    fs.readFile(Vieux_Lyon_Historique, function(err, data) {
       if (err) {
         res.send(err);
       } else {
